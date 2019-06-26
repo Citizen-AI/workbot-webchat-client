4c95b868e0988e4891e02899e29a32f6ecd3aa6b
Matthew Bartlett <mhjbnz@gmail.com>
2019-06-26 15:58:21 +1200

    Make paths relative

--- client.js
+++ client.js
@@ -7,7 +7,7 @@ converter.setOption('openLinksInNewWindow', true);
 
 var Botkit = {
     config: {
-        ws_url: (location.protocol === 'https:' ? 'wss' : 'ws') + '://' + location.hostname + ':3000',
+        ws_url: (location.protocol === 'https:' ? 'wss' : 'ws') + '://rentbot-webchat-server.herokuapp.com',
         reconnect_timeout: 3000,
         max_reconnect: 5,
         enable_history: false,
